2ef00b3251f0eb406ec05856125091edb5e08edc
dohyeonlee <dohyeonlee@toss.im>
2023-12-29 00:35:56 +0900

    feat: env 설정 추가 및 mysql env로 분기

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './modules/users/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: '1234',
-      database: 'dotoriham-db',
-      synchronize: true,
+      username: process.env.MYSQL_DB_USERNAME,
+      password: process.env.MYSQL_DB_PASSWORD,
+      database: process.env.MYSQL_DB_DATABASE,
+      synchronize: false,
+      entities: [UserEntity],
     }),
     UsersModule,
   ],
