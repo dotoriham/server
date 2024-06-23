5ed3120948fe390381068064ae58b2321865c8b8
ksmfou98 <ksmfou98@naver.com>
2024-06-24 00:18:07 +0900

    feat: auth module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { UserModule } from './modules/user/user.module';
     }),
     MongooseModule.forRoot(process.env.MONGO_URI),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
